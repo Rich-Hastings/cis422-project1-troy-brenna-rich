e4b6b2125daa4f91f5cd6544e3ea9e90c2a22496
Rich-Hastings <72363497+Rich-Hastings@users.noreply.github.com>
2020-10-06 17:47:37 -0700

    Update getLocation.py
    removed key

--- getLocation.py
+++ getLocation.py
@@ -5,7 +5,7 @@ def getLocation(lat, lon):
     """helper function to get address from lat and lon"""
     # geolocator = Nominatim(user_agent="project1") #remove comment for Nominatim
     location = str(lat) + "," + " "+str(lon)
-    geolocator = GoogleV3(api_key="AIzaSyDbc-uvarJSL3JSaNkyO1lCDQoawBpRdhM") #assigns key for search
+    geolocator = GoogleV3(api_key="add key here") #assigns key for search
     location = geolocator.reverse(location)
     return location.address
 
